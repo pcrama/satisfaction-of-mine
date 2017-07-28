490d8f521d8d927ddc29a3b3b4aebbdee9f62f93
Philippe Crama <pcfeb0009@gmx.com>
2017-07-28 23:29:43 +0200

    Implement rule evaluation

--- src/rules.py
+++ src/rules.py
@@ -1,20 +1,104 @@
 """Interpret rules to compute satisfaction score of a given time entry"""
 
+from abc import ABCMeta, abstractmethod
+
 import attr
+from typing import Any, Optional, Sequence, TypeVar
 
 import entries
 
+
+class Rule(metaclass=ABCMeta):
+    "Abstract class for mypy type declaration purposes"
+    @abstractmethod
+    def match(self, e: entries.TimeEntry) -> Optional[float]:
+        pass
+
+    def _validate_weight(self, attribute, value):
+        attr.validators.instance_of(float)(self, attribute, value)
+        if value < 0 or value > 1.0:
+            raise ValueError("'{name}' must be in [0.0..1.0] and {value!r} isn't".
+                             format(name=attribute.name, value=value))
+
+
 @attr.s
-class MatchIssueID(object):
+class MatchAny(Rule):
+    weight = attr.ib(validator=Rule._validate_weight)
+
+    def match(self, e):
+        return self.weight
+
+
+@attr.s
+class MatchIssueID(Rule):
     issue_id = attr.ib(validator=attr.validators.instance_of(str))
+    weight = attr.ib(validator=Rule._validate_weight)
 
-    def match(self, e: entries.TimeEntry) -> bool:
-        return e.issue_id == self.issue_id
+    def match(self, e):
+        if e.issue_id == self.issue_id:
+            return self.weight
 
 
 @attr.s
-class MatchCategory(object):
+class MatchCategory(Rule):
     category = attr.ib(validator=attr.validators.instance_of(str))
+    weight = attr.ib(validator=Rule._validate_weight)
 
-    def match(self, e: entries.TimeEntry) -> bool:
+    def match(self, e):
         return e.category == self.category
+
+
+class Selector(metaclass=ABCMeta):
+    """Search the sequence of rules for the rule matching the L{TimeEntry}
+
+    Rather than passing a C{callable}, the function is wrapped in a class with
+    a single L{select} method."""
+    # The abstractmethod stuff is to help mypy
+    @abstractmethod
+    def select(self, rules: Sequence[Rule], entry: entries.TimeEntry) -> Rule:
+        pass
+
+
+class Accumulator(metaclass=ABCMeta):
+    """Represent data with a neutral element and associative operation
+
+    Example: (0, +) or (1, *)"""
+    # The abstractmethod stuff is to help mypy
+    @classmethod
+    @abstractmethod
+    def neutral(cls) -> "Accumulator":
+        "Return fresh Accumulator instance that can be combined with others"
+        pass
+
+    @abstractmethod
+    def update(self, other) -> None:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def from_rule_and_entry(cls, rule: Rule, entry: entries.TimeEntry) -> "Accumulator":
+        pass
+
+
+class Convertor(metaclass=ABCMeta):
+    """Convert a L{TimeEntry} into an L{Accumulator}
+
+    Rather than passing a C{callable}, the function is wrapped in a class with
+    a single L{select} method."""
+    # The abstractmethod stuff is to help mypy
+    @abstractmethod
+    def convert(self, entry: entries.TimeEntry) -> Any:
+        pass
+
+
+@attr.s
+class RuleEvaluator(object):
+    selector = attr.ib(validator=attr.validators.instance_of(Selector))
+    accumulator = attr.ib(validator=attr.validators.instance_of(Accumulator))
+
+    def satisfaction(self, rules: Sequence[Rule], entries: Sequence[entries.TimeEntry]) -> Accumulator:
+        acc = self.accumulator.neutral()
+        for entry in entries:
+            r = self.selector.select(rules, entry)
+            acc.update(self.accumulator.from_rule_and_entry(r, entry))
+        return acc
--- src/test_rules.py
+++ src/test_rules.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import unittest
 
+import attr
+
 import entries
 import rules
 
@@ -12,48 +14,129 @@ def _make_TimeEntry(issue_id="12345", duration=1.0, category="Test",
 class TestMatchIssueID(unittest.TestCase):
     def test_001_create_MatchIssueID(self):
         "You can create a rule matching a precise issue ID but only using a str"
-        with self.assertRaises(TypeError):
-            r = rules.MatchIssueID(1234)
+        with self.assertRaisesRegex(TypeError, "issue_id"):
+            r = rules.MatchIssueID(1234, 1.0)
 
     def test_002_create_MatchIssueID(self):
         "You can create a rule matching a precise issue ID"
-        r = rules.MatchIssueID("01234")
+        r = rules.MatchIssueID("01234", 1.0)
 
-    def test_003_use_MatchIssueID(self):
+    def test_003_create_MatchIssueID(self):
+        "MatchIssueID validates the weight attribute"
+        param = "weight"
+        create_with_weight = lambda w: rules.MatchIssueID("1234", w)
+        with self.assertRaisesRegex(TypeError, "weight"):
+            r = create_with_weight(None)
+        with self.assertRaisesRegex(ValueError, "weight"):
+            r = create_with_weight(-0.1)
+        with self.assertRaisesRegex(ValueError, "weight"):
+            r = create_with_weight(1.1)
+
+    def test_100_use_MatchIssueID(self):
         "A matching issue ID in a time entry is recognized"
         issue_id = "12345"
-        r = rules.MatchIssueID(issue_id)
+        weight = 0.23
+        r = rules.MatchIssueID(issue_id, weight)
         e = _make_TimeEntry(issue_id=issue_id)
-        self.assertTrue(r.match(e))
+        self.assertEqual(weight, r.match(e))
 
-    def test_004_use_MatchIssueID(self):
+    def test_101_use_MatchIssueID(self):
         "A mismatching issue ID in a time entry is rejected"
-        r = rules.MatchIssueID("12345")
+        r = rules.MatchIssueID("12345", 1.0)
         e = _make_TimeEntry(issue_id="6789")
-        self.assertFalse(r.match(e))
+        self.assertIs(None, r.match(e))
 
 
 class TestMatchCategory(unittest.TestCase):
     def test_001_create_MatchCategory(self):
         "MatchCategory validates its target category"
         with self.assertRaisesRegex(TypeError, "category"):
-            r = rules.MatchCategory(1234)
+            r = rules.MatchCategory(1234, 1.0)
 
     def test_002_create_MatchCategory(self):
-        r = rules.MatchCategory("Automation")
+        r = rules.MatchCategory("Automation", 1.0)
 
-    def test_003_use_MatchCategory(self):
+    def test_003_create_MatchCategory(self):
+        "MatchCategory validates its weight"
+        with self.assertRaisesRegex(TypeError, "weight"):
+            r = rules.MatchCategory("Automation", None)
+
+    def test_004_create_MatchCategory(self):
+        "MatchCategory validates its weight"
+        with self.assertRaisesRegex(ValueError, "weight"):
+            r = rules.MatchCategory("Automation", -0.1)
+        with self.assertRaisesRegex(ValueError, "weight"):
+            r = rules.MatchCategory("Automation", 1.1)
+
+    def test_100_use_MatchCategory(self):
         "A matching category in a time entry is recognized"
         category = "12345"
-        r = rules.MatchCategory(category)
+        r = rules.MatchCategory(category, 1.0)
         e = _make_TimeEntry(category=category)
         self.assertTrue(r.match(e))
 
-    def test_004_use_MatchCategory(self):
+    def test_101_use_MatchCategory(self):
         "A mismatching category in a time entry is rejected"
-        r = rules.MatchCategory("12345")
+        r = rules.MatchCategory("12345", 1.0)
         e = _make_TimeEntry(category="6789")
         self.assertFalse(r.match(e))
+
+
+@attr.s
+class _DummyAccumulator(rules.Accumulator):
+    x = attr.ib(default=[])
+
+    @classmethod
+    def neutral(cls):
+        return cls([])
+    
+    def update(self, other: "_DummyAccumulator") -> None:
+        self.x.extend(other.x)
+
+    @classmethod
+    def from_rule_and_entry(cls, rule, entry):
+        return cls([(rule, entry)])
+
+
+class _DummySelector(rules.Selector):
+    def select(self, rules, entry):
+        return next(r for r in rules if r.match(entry))
+
+
+class TestRuleEvaluator(unittest.TestCase):
+    def test_001_create_RuleEvaluator(self):
+        "RuleEvaluator validates its `selector' parameter"
+        with self.assertRaisesRegex(TypeError, "selector"):
+            rules.RuleEvaluator(None, _DummyAccumulator())
+
+    def test_002_create_RuleEvaluator(self):
+        "RuleEvaluator validates its `accumulator' parameter"
+        with self.assertRaisesRegex(TypeError, "accumulator"):
+            rules.RuleEvaluator(_DummySelector(), None)
+
+    def test_003_create_RuleEvaluator(self):
+        "Create a RuleEvaluator"
+        rules.RuleEvaluator(_DummySelector(), _DummyAccumulator())
+
+    def test_004_use_RuleEvaluator(self):
+        rulz = [rules.MatchIssueID("65432", 0.9),
+                rules.MatchCategory("Automation", 1.0),
+                rules.MatchIssueID("12345", 0.6),
+                rules.MatchAny(0.5)]
+        entrz = [entries.TimeEntry("12345", 1.0, "Automation", "", "2017-07-29"),
+                 entries.TimeEntry("12345", 1.0, "Other", "", "2017-07-29"),
+                 entries.TimeEntry("65432", 1.0, "Other", "", "2017-07-29"),
+                 entries.TimeEntry("76543", 1.0, "Automation", "", "2017-07-29"),
+                 entries.TimeEntry("65432", 1.0, "Automation", "", "2017-07-29"),
+                 entries.TimeEntry("76543", 1.0, "Other", "", "2017-07-29")]
+        evaluator = rules.RuleEvaluator(_DummySelector(), _DummyAccumulator())
+        self.assertEqual([(rulz[1], entrz[0]),
+                          (rulz[2], entrz[1]),
+                          (rulz[0], entrz[2]),
+                          (rulz[1], entrz[3]),
+                          (rulz[0], entrz[4]),
+                          (rulz[3], entrz[5])],
+                         evaluator.satisfaction(rulz, entrz).x)
 
 
 if __name__ == "__main__":
